34b8ffc3676899d1acb0950cade1f77ece985914
Kai Peng Li <oopanpan@gmail.com>
2020-12-24 23:14:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,39 @@
-# Write your code here!
+require 'pry'
+
+def game_hash
+    { 
+    :home => {
+      :team_name => "" ,
+      :colors =>[] ,
+      :players => [{
+        :player_name => "",
+        :number => 0,
+        :shoe => 0, 
+        :points => 0,
+        :rebounds => 0, 
+        :assists => 0,
+        :steals => 0, 
+        :blocks => 0,
+        :slam_dunks => 0, 
+      }]
+    }, 
+    :away => {
+      :team_name => "" ,
+      :colors =>[] ,
+      :players => [{
+        :player_name => "",
+        :number => 0,
+        :shoe => 0, 
+        :points => 0,
+        :rebounds => 0, 
+        :assists => 0,
+        :steals => 0, 
+        :blocks => 0,
+        :slam_dunks => 0, 
+      }]
+    }
+  }
+  #binding.pry
+end
+
+puts game_hash
